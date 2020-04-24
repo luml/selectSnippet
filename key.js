2ea663fbff85d09be73c24b40f7bbb1a8816fd63
luml <1520622848@qq.com>
2020-04-24 15:44:12 +0800

    feat(OOP)

--- key.js
+++ key.js
@@ -1,17 +1,41 @@
-let articleDom = document.createElement("article");
-let articleJs = document.createElement("article");
-articleDom.style.color = "white";
+// Create two article dom for left and right part of window
+
+class articleMaking {
+  constructor(name, color){
+    this.dom = document.createElement(name);
+    this.name = name;
+    this.color = color;
+  }
+  colorit() {
+    this.dom.style.color = this.color;
+  }
+  contontIt(value) {
+    if(value){
+      this.dom.innerText = value;
+    }else{
+      this.dom.innerText = null
+    }
+  }
+}
+
+const arty_1 = new articleMaking("article", "white")
+arty_1.colorit()
+
+const arty_2 = new articleMaking("article", "green")
+arty_2.colorit()
+
 const target = document.getElementById("findTarget");
 const contentFill = document.querySelector(".content");
 const pickDialog = document.querySelector('.pickDialog')
 
-const demodom1 = `<div class='demo' id='demo'>
+const demodom = `<div class='demo' id='demo'>
                     <ul>
                       <li>baseball</li>
                       <li class='li-2'>tennis</li>
                       <li>soccer</li>
                     </ul>
                   </div>`;
+
 const demojs1 = `document.querySelector('div ul li.li-2')`;
 const demojs2 = `document.querySelectorAll('div ul li')`;
 const demojs3 = `document.getElementById('demo').classList.add('demo')
@@ -31,51 +55,49 @@ const demojs5 = `
                   node.value = 5;
                   document.querySelector('div ul').appendChild(node);
                 `;
-
 const demojs6 = `<div><a href="https://grrr.tech/posts/create-dom-node-from-html-string/"></a></div>`;
 const demojs7 = `<div><p>See more at https://grrr.tech/posts/create-dom-node-from-html-string/</p></div>`;
 
 
 target.onchange = function () {
-  articleDom.innerText = demodom1;
-  articleJs.style.color = "#0dec7b";
+  arty_1.dom.innerText = demodom;
   switch (target.value) {
     case "1":
-      articleJs.innerText = demojs1;
+      arty_2.contontIt(demojs1)
       break;
     case "2":
-      articleJs.innerText = demojs2;
+      arty_2.contontIt(demojs2)
       break;
     case "3":
-      articleJs.innerText = demojs3;
+      arty_2.contontIt(demojs3)
       break;
     case "4":
-      articleJs.innerText = demojs4;
+      arty_2.contontIt(demojs4)
       break;
     case "5":
-      articleJs.innerText = demojs5;
+      arty_2.contontIt(demojs5)
       break
     case "6":
-      articleJs.innerText = ''
+      arty_2.contontIt()
       const divDom6 = new DOMParser().parseFromString(demojs6, 'text/html').body.firstElementChild;
       const bubble6 = makeBubble()
       bubble6.innerText = `You just created ` + divDom6.nodeName + ">" + divDom6.firstElementChild.nodeName;
-      articleJs.appendChild(bubble6)
+      arty_2.dom.appendChild(bubble6)
       break;
     case "7":
-      articleJs.innerText = ''
+      arty_2.contontIt()
       const divDom7 = document.createRange().createContextualFragment(demojs7).firstElementChild;
       const bubble7 = makeBubble();
       bubble7.innerText = `You just created ` + divDom7.nodeName + ">" + divDom7.firstElementChild.nodeName
-      articleJs.appendChild(bubble7)
+      arty_2.dom.appendChild(bubble7)
       break
     default:
-      articleJs.textContent = ''
+      arty_2.contontIt()
       pickDialog.showModal()
       pickDialog.firstElementChild.textContent = `No item selected`
   }
-  contentFill.insertAdjacentElement("beforeend", articleDom)
-  articleDom.insertAdjacentElement("afterend", articleJs)
+  contentFill.insertAdjacentElement("beforeend", arty_1.dom)
+  arty_1.dom.insertAdjacentElement("afterend", arty_2.dom)
 };
 
 window.addEventListener('load', function () {
